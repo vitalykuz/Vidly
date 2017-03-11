1b720371e70fbd0a4e557d705bf846e32dc1ab98
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-03-11 18:29:19 +1100

    Add ApplicationDbContext as _context to the Customer Controller
    Submitted some data into Customer table

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -10,29 +10,36 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        
+        //we need to have db context for using the database.
+        //By convention we create private property and initialise the property in default constructor
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // we need to properly dispose the _context object. Proper way:
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            // the query is only executed when we iterate through each customer (it does it in View)
+            // var customers = _context.Customers; return View(customers)
+            // we can add .ToList() and the query is executed.
+            var customers = _context.Customers;
 
             return View(customers);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "Vitaly Kuzenkov"},
-                new Customer {Id = 2, Name = "Ivan and Vova"},
-                new Customer {Id = 3, Name = "Tanya"}
-            };
         }
 
         // GET: Customers/Details
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
@@ -41,6 +48,19 @@ namespace Vidly.Controllers
 
             return View(customer);
         }
-
     }
 }
+
+
+// we dont need this method anymore. I just want to keep it commented as reference.
+/*
+ * private IEnumerable<Customer> GetCustomers()
+    {
+        return new List<Customer>
+        {
+            new Customer {Id = 1, Name = "Vitaly Kuzenkov"},
+            new Customer {Id = 2, Name = "Ivan and Vova"},
+            new Customer {Id = 3, Name = "Tanya"}
+        };
+    }
+ */
