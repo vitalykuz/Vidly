549d1c2f00de021cfcb5dc8531624367eafc5b2b
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-03-11 18:12:58 +1100

    Apply Annotations to Customer Name

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,16 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
+        //if you want to make a property not nullable in database, add required. Now name cant be null and the lenth = 255
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
         //by convention (name of the class+id?), the framework recognises this property as foreign key.
         public byte MembershipTypeId { get; set; }
     }
