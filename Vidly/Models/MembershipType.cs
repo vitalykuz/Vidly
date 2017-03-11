9abe36619a1989dd55ccec4f17ba13d9748813dd
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-03-11 17:57:11 +1100

    Populated the MemebershipTypes table according to business rules

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Business rules for membership types:
+ * https://www.udemy.com/the-complete-aspnet-mvc-5-course/learn/v4/t/lecture/4873034?start=0
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,7 +13,7 @@ namespace Vidly.Models
 {
     public class MembershipType
     {
-        //each entity mush have a key, it will a primary key in database. By convention it must be named either id or type+id
+        //each entity mush have a key, it will be a primary key in database. By convention it must be named either id or type+id
         public byte Id { get; set; } //membership type Id
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
