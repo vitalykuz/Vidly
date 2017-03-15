8ca2dda9639d7822957b902d502ad3d45bbd0fa2
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-03-15 18:22:19 +1100

    Start working on API. Just added a CustomersController

---
+++ Vidly/Controllers/API/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.API
+{
+    public class CustomersController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+    }
+}
