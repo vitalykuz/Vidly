0dd85663bdb6cd24a6ce97a1408cbfe3fcb0e3f1
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-03-15 20:42:53 +1100

    Add API Post Delete Put and Get
    Tested API in Postman

--- Vidly/Controllers/API/CustomersController.cs
+++ Vidly/Controllers/API/CustomersController.cs
@@ -17,10 +17,85 @@ namespace Vidly.Controllers.API
             _context = new ApplicationDbContext();
         }
 
-        // GET api/customers
+        // GET /api/customers
         public IEnumerable<Customer> GetCustomers()
         {
             return _context.Customers.ToList();
         }
+
+        // GET /api/customers/1
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            // this is a convention. 
+            // If the given resource is not found we throw the standart Not Found Httpresponce
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return customer;
+        }
+
+        /*  POST /api/customers
+         by convention we return a new resource (in ths case a new customer)
+         If we name a method like CreateCustomer, it's not by convention so we must add [HttpPost] above the method. This method is better.
+         But if we name the method as PostCustomer then the asp.net does know that it's a post method (dont need to specify [HttpPost]). official Microsoft approach.
+         */
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer) // this customer comes form a request body
+        {
+            // this is a convention.  we throw the standart Bad Request HttpResponce
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // PUT /api/customers/1
+        // two returns options available: return a resource(in this case a customer) or void
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer) // this customer comes form a request body
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            customerInDb.Name = customer.Name;
+            customerInDb.Birthday = customer.Birthday;
+            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customerInDb);
+
+            _context.SaveChanges();
+        }
+
     }
 }
---
+++ Vidly/DTOs/CustomerDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class CustomerDto
+    {
+        //primary key
+        public int Id { get; set; }
+
+        //if you want to make a property not nullable in database, add required. Now name cant be null and the lenth = 255
+        [Required(ErrorMessage = "Please provide customer's name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        //by convention (name of the class+id?), the framework recognises this property as foreign key.
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
+        public DateTime? Birthday { get; set; }
+    }
+}
